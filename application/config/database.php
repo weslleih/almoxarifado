4972d071a5ec0f2f7b8cca14b43f87328f1c24d1
Wesllei Rosário <wesllei.h@gmail.com>
2014-08-15 16:36:55 -0400

    Rename databese sample file

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'dbuser';
-$db['default']['password'] = 'dbpass';
-$db['default']['database'] = 'dbname';
+$db['default']['username'] = '';
+$db['default']['password'] = '';
+$db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
